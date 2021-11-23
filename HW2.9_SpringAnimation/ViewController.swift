d197d9b38bcd59fa90bdbc8e90aabfe0d97109af
Stanislav Buinovsky <sbuinovsky@gmail.com>
2021-11-23 22:40:25 +0300

    Initial Commit

---
+++ HW2.9_SpringAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.9_SpringAnimation
+//
+//  Created by Станислав Буйновский on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
